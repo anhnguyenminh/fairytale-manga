ce1a3f2a43e50b0392be9a23a8b490d276a95c74
nvhoang2003 <nvhoang12082003@gmail.com>
2022-09-26 21:16:37 +0700

    Complete Authorized Admin

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
+require 'json_web_token'
+
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+    def authenticate_request
+      header = request.headers["Authorization"]
+      if header
+        header = header.split(" ").last 
+        decode = JsonWebToken.decode(header)
+        @current_admin= Admin.find_by(id: decode[:admin_id])
+      else
+        render json: {
+          message: "You need to login"
+        }
+      end
+    end
 end
