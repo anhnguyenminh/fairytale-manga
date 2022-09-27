ac95ba26dae69b6fe07a4f7297d6b4f52512342b
nvhoang2003 <nvhoang12082003@gmail.com>
2022-09-27 09:41:22 +0700

    Create Join Table With Reader And Gift

---
+++ Backend/db/migrate/20220927023752_create_join_table_gift_reader.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableGiftReader < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :reader, :gift do |t|
+      t.index :reader_id
+      t.index :gift_id
+      t.timestamps
+  end
+end
