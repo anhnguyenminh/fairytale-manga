e3b84879fb288ff5b7d1e57a3e4184bf13d1532c
nvhoang2003 <nvhoang12082003@gmail.com>
2022-09-27 11:04:16 +0700

    Fix Bug Turn Off Auth in applicationcontroller

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -4,16 +4,16 @@ class ApplicationController < ActionController::API
   # before_action :authenticate_request
 
   private
-    def authenticate_request
-      header = request.headers["Authorization"]
-      if header
-        header = header.split(" ").last 
-        decode = JsonWebToken.decode(header)
-        @current_admin= Admin.find_by(id: decode[:admin_id])
-      else
-        render json: {
-          message: "You need to login"
-        }
-      end
-    end
+    # def authenticate_request
+    #   header = request.headers["Authorization"]
+    #   if header
+    #     header = header.split(" ").last 
+    #     decode = JsonWebToken.decode(header)
+    #     @current_admin= Admin.find_by(id: decode[:admin_id])
+    #   else
+    #     render json: {
+    #       message: "You need to login"
+    #     }
+    #   end
+    # end
 end
