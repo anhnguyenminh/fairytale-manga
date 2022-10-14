63f474a119538f90e92649ce186bd45c8eda9e83
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-14 17:09:57 +0700

    Add status for FE hien thi cho nguoi dung

--- Backend/app/serializers/story_serializer.rb
+++ Backend/app/serializers/story_serializer.rb
@@ -1,3 +1,11 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :author, :end, :chapter
+  attributes :id, :name, :author, :status, :chapter
+
+  def status
+    if object.end == false
+      return "end"
+    else
+      return "on going"
+    end
+  end
 end
