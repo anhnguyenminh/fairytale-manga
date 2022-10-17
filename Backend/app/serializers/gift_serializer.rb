e131b07e048620b161cfa981dea71c95d83d2cd9
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-17 11:44:31 +0700

    Fix bug

--- Backend/app/serializers/gift_serializer.rb
+++ Backend/app/serializers/gift_serializer.rb
@@ -1,6 +1,6 @@
 class GiftSerializer < ActiveModel::Serializer
   # include JSONAPI::serializer
-  attributes :is, :name, :score, :stock, :image_url #, :status
+  attributes :id, :name, :score, :stock, :image_url #, :status
 
   # has_many :reader
   has_many :reader_gift, serializer: ReaderGiftSerializer
