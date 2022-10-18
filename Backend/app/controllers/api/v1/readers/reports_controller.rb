b465c6d4b60326f6786d57baf982ffee9b5b7727
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-18 10:38:12 +0700

    Add create report

--- Backend/app/controllers/api/v1/readers/reports_controller.rb
+++ Backend/app/controllers/api/v1/readers/reports_controller.rb
@@ -4,17 +4,18 @@ module Api
       class ReportsController < ReaderappController
         def createreports
           @report = Report.find_by(reader_id: current_reader.id, comment_id: params[:id])
-          @comment = Comment.find(params[:id])
-          @comment.update(content: @comment.content= "asdad")
-          # if(@comment.reader_id == current_reader.id)
-          #   render json: "You can't report your comment"
-          # elsif @report
-          #   render json: "You can report 1 time 1 comment"
-          # else
-            
-          # end
-          # render json: @comment.reader_id
-          # render json: current_reader
+          @comment = Comment.find(params[:id])        
+          if(@comment.reader_id == current_reader.id)
+            render json: "You can't report your comment"
+          elsif @report
+            render json: "You can report 1 time 1 comment"
+          else
+            @comment.update(num_report: @comment.num_report+1 )
+            Report.create(reader_id: current_reader.id, comment_id: params[:id])
+            render json: {
+              message: "We will check your report"
+            }
+          end
         end
       end
     end
