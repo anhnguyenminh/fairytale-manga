aac76e050089b2c44c055d636c89dc9bb15e07bd
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-24 10:39:56 +0700

    Change client gift

--- Backend/app/controllers/api/v1/readers/gifts_controller.rb
+++ Backend/app/controllers/api/v1/readers/gifts_controller.rb
@@ -3,23 +3,15 @@ module Api
     module Readers
       class GiftsController < ReaderappController
         def index
-          # gifts = Gift.order(id: :desc).ransack(params[:q]).result
-          # @pagy, @gifts = pagy(gifts, items: 2)
           @gifts = Gift.all
-          message = "asda"
-          current_reader = @current_reader
-          # binding.pry
-
-          # response_list(@gifts, { adapter: :json,
-          #                         each_serializer: GiftSerializer })
-          render ({ json: @gifts, adapter: :json, each_serializer: GiftSerializer, current_reader: @current_reader.id })
-          #  render json: current_reader
-
+          # render ({ json: @gifts, adapter: :json, each_serializer: GiftSerializer, current_reader: current_reader.id })
+          response_success(@gift, {each_serializer: GiftSerializer}, current_reader.id)
         end
 
         def show
           @gift = Gift.find(params[:id])
-          render json: @gift, :each_serializer => GiftSerializer
+          # render json: @gift, :each_serializer => GiftSerializer
+          response_success(@gift, {each_serializer: GiftSerializer})
         end
 
         # def exchange
