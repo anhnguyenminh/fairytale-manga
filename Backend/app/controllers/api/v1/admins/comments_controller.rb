5d8d98ea269ea854803f0e8784899b0540a0ef6c
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-24 09:18:31 +0700

    Change Comment

--- Backend/app/controllers/api/v1/admins/comments_controller.rb
+++ Backend/app/controllers/api/v1/admins/comments_controller.rb
@@ -3,31 +3,36 @@ module Api
     module Admins
       class CommentsController < ApplicationController
         def index
-          comment = Comment.where("num_report >= ? and status = 0", 3).order(id: :desc).ransack(params[:q]).result
+          # comment = Comment.where("num_report >= ? and status = 0", 3).order(id: :desc).ransack(params[:q]).result
+          comment = Comment.comment_be_report
           @pagy, @comment = pagy(comment, items: 10)
           response_list(@comment, { adapter: :json,
                                        each_serializer: nil })
         end
 
         def ban_comment
-          @comment = Comment.find(params[:id])
+          comment = Comment.find(params[:id])
           # @comment.update(status: params[:status])
-          if params[:status] == 1 #banned
+          if params[:status] == 1 #user enter key banned
             @reader = Reader.find(@comment.reader_id)
             @comment.update(status: params[:status])
             @title = "Warning your comment is toxic. We will ban you.!!"
             @notification = Notification.create(reader_id: @comment.reader_id, title: @title)
             if @reader.update(time_ban: Time.now)
-              render json: "dr"
+              # render json: "dr"
+              response_success("Ban success")
             else 
-              render json: "chua dc"
+              # render json: "chua dc"
+              response_error("Failed")
             end
           elsif params[:status] == 2
-            render json: "ok"
+            # render json: "ok"
+            response_success("Don't ban success")
           else
-            render json: {
-              message: "Not have status"
-            }, status: 400
+            # render json: {
+            #   message: "Not have status"
+            # }, status: 400
+            response_error(message: "Please enter status")
           end
         end
       end
--- Backend/app/models/comment.rb
+++ Backend/app/models/comment.rb
@@ -4,4 +4,11 @@ class Comment < ApplicationRecord
   has_many :comment, as: :commentable
   has_many :like, as: :liketable
   has_many :report 
+
+  def self.comment_be_report
+    comment = Comment.where("num_report >= ? and status = 0", 3)
+                     .order(id: :desc)
+                     .ransack(params[:q])
+                     .result
+  end
 end
