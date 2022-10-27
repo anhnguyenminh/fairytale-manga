fbe944752c6879efeedaddd14a3db0f1e528c561
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-27 09:35:07 +0700

    -m

--- Backend/app/controllers/api/v1/readers/hotstories_controller.rb
+++ Backend/app/controllers/api/v1/readers/hotstories_controller.rb
@@ -9,21 +9,21 @@ module Api
           time_range = (Time.now - 1.day+ 7.hour)..Time.now+7.hour
           @stories = Story.hot_story(time_range)
           # render json: @stories, each_serializer: nil
-          response_success(@stories)
+          response_success(@stories, {each_serializer: ::Stories::ShowStoriesSerializer})
         end
 
         def hot_month
           time_range = (Time.now.midnight - 1.month)..Time.now.midnight
           @stories = Story.hot_story(time_range)
           # render json: @stories, each_serializer: nil
-          response_success(@stories)
+          response_success(@stories, {each_serializer: ::Stories::ShowStoriesSerializer})
         end
 
         def hot_week
           time_range = (Time.now.midnight - 1.week)..Time.now.midnight
           @stories = Story.hot_story(time_range)
           # render json: @stories, each_serializer: nil
-          response_success(@stories)
+          response_success(@stories, {each_serializer: ::Stories::ShowStoriesSerializer})
         end
       end
     end
