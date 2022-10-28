e4353b9268d49d1037784820e07ff8ef24a21b63
nvhoang2003 <nvhoang12082003@gmail.com>
2022-10-28 14:46:18 +0700

    asdad

--- Backend/app/controllers/api/v1/readers/recommends_controller.rb
+++ Backend/app/controllers/api/v1/readers/recommends_controller.rb
@@ -3,14 +3,15 @@ module Api
     module Readers
       class RecommendsController < ApplicationController
         def recommend_story
-          @stories = Story.order("created_at DESC").limit(10)
+          @stories = Story.joins(:chapter).uniq.order("created_at DESC").limit(10)
           # render json: @stories, each_serializer: nil
-          response_success(@stories)
+          response_success(@stories, serializer: ::Reader::StorySerializer)
         end
 
         def get_story_by_category
           @stories = Story.joins(:category)
                           .where("category_story.category_id= ?", params[:id])
+                          .joins(:chapter).uniq
           response_success(@stories, serializer: ::Reader::StorySerializer)
         end
       end
